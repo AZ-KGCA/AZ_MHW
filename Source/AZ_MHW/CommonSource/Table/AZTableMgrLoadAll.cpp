7299a0f5570e3e5a110bd7a12a7efeacb1bdf610
AshOne91 <ldgo91@gmail.com>
2023-05-11 23:30:06 +0900

    feat : gen total table

--- Source/AZ_MHW/CommonSource/Table/AZTableMgrLoadAll.cpp
+++ Source/AZ_MHW/CommonSource/Table/AZTableMgrLoadAll.cpp
@@ -1,33 +1,25 @@
 #include "AZ_MHW/Manager/AZTableMgr.h"
 
 #include "AZ_MHW/Util/AZUtility.h"
-#include "AZ_MHW/CommonSource/Table/ConstantData.h"
-#include "AZ_MHW/CommonSource/Table/TestData.h"
 #include "AZ_MHW/CommonSource/Table/BossData.h"
 #include "AZ_MHW/CommonSource/Table/BottleData.h"
 #include "AZ_MHW/CommonSource/Table/CommandData.h"
+#include "AZ_MHW/CommonSource/Table/ConstantData.h"
 #include "AZ_MHW/CommonSource/Table/DungeonData.h"
 #include "AZ_MHW/CommonSource/Table/ItemBuffData.h"
 #include "AZ_MHW/CommonSource/Table/LoadingData.h"
 #include "AZ_MHW/CommonSource/Table/MapData.h"
 #include "AZ_MHW/CommonSource/Table/ModelData.h"
+#include "AZ_MHW/CommonSource/Table/MonsterCombatActionData.h"
+#include "AZ_MHW/CommonSource/Table/MonsterData.h"
 #include "AZ_MHW/CommonSource/Table/MonsterNonCombatActionData.h"
 #include "AZ_MHW/CommonSource/Table/PotionData.h"
 #include "AZ_MHW/CommonSource/Table/SpawnData.h"
+#include "AZ_MHW/CommonSource/Table/TestData.h"
 #include "AZ_MHW/CommonSource/Table/TotalItemData.h"
-#include "AZ_MHW/CommonSource/Table/MonsterData.h"
-#include "AZ_MHW/CommonSource/Table/MonsterCombatActionData.h"
 
 void UAZTableMgr::LoadAll()
 {
-	if (!_Load<UConstantData>("ConstantData.csv"))
-	{
-		UAZUtility::ShippingLog(FString::Printf(TEXT("[UConstantData] Load Fail!")));
-	}
-	if (!_Load<UTestData>("TestData.csv"))
-	{
-		UAZUtility::ShippingLog(FString::Printf(TEXT("[UTestData] Load Fail!")));
-	}
 	if (!_Load<UBossData>("BossData.csv"))
 	{
 		UAZUtility::ShippingLog(FString::Printf(TEXT("[UBossData] Load Fail!")));
@@ -39,6 +31,10 @@ void UAZTableMgr::LoadAll()
 	if (!_Load<UCommandData>("CommandData.csv"))
 	{
 		UAZUtility::ShippingLog(FString::Printf(TEXT("[UCommandData] Load Fail!")));
+	}
+	if (!_Load<UConstantData>("ConstantData.csv"))
+	{
+		UAZUtility::ShippingLog(FString::Printf(TEXT("[UConstantData] Load Fail!")));
 	}
 	if (!_Load<UDungeonData>("DungeonData.csv"))
 	{
@@ -60,6 +56,14 @@ void UAZTableMgr::LoadAll()
 	{
 		UAZUtility::ShippingLog(FString::Printf(TEXT("[UModelData] Load Fail!")));
 	}
+	if (!_Load<UMonsterCombatActionData>("MonsterCombatActionData.csv"))
+	{
+		UAZUtility::ShippingLog(FString::Printf(TEXT("[UMonsterCombatActionData] Load Fail!")));
+	}
+	if (!_Load<UMonsterData>("MonsterData.csv"))
+	{
+		UAZUtility::ShippingLog(FString::Printf(TEXT("[UMonsterData] Load Fail!")));
+	}
 	if (!_Load<UMonsterNonCombatActionData>("MonsterNonCombatActionData.csv"))
 	{
 		UAZUtility::ShippingLog(FString::Printf(TEXT("[UMonsterNonCombatActionData] Load Fail!")));
@@ -72,16 +76,12 @@ void UAZTableMgr::LoadAll()
 	{
 		UAZUtility::ShippingLog(FString::Printf(TEXT("[USpawnData] Load Fail!")));
 	}
+	if (!_Load<UTestData>("TestData.csv"))
+	{
+		UAZUtility::ShippingLog(FString::Printf(TEXT("[UTestData] Load Fail!")));
+	}
 	if (!_Load<UTotalItemData>("TotalItemData.csv"))
 	{
 		UAZUtility::ShippingLog(FString::Printf(TEXT("[UTotalItemData] Load Fail!")));
-	}
-	if (!_Load<UMonsterData>("MonsterData.csv"))
-	{
-		UAZUtility::ShippingLog(FString::Printf(TEXT("[UMonsterData] Load Fail!")));
-	}
-	if (!_Load<UMonsterCombatActionData>("MonsterCombatActionData.csv"))
-	{
-		UAZUtility::ShippingLog(FString::Printf(TEXT("[UMonsterCombatActionData] Load Fail!")));
 	}
 }
