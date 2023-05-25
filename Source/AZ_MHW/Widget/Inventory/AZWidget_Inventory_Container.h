e50a1c365cf369773ddf9d52dfc4149492f05c1e
AshOne91 <ldgo91@gmail.com>
2023-05-25 15:48:53 +0900

    feat : add item inventory

---
+++ Source/AZ_MHW/Widget/Inventory/AZWidget_Inventory_Container.h
@@ -0,0 +1,17 @@
+// Copyright Team AZ. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AZ_MHW/Widget/AZWidget.h"
+#include "AZWidget_Inventory_Container.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AZ_MHW_API UAZWidget_Inventory_Container : public UAZWidget
+{
+	GENERATED_BODY()
+	
+};
