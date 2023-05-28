99bedde68fabca777005c68dad134d046a936ba5
AshOne91 <ldgo91@gmail.com>
2023-05-29 06:25:32 +0900

    feat : add popupdamageinfo

---
+++ Source/AZ_MHW/Widget/InGame/AZWidget_PopupDamageInfo.h
@@ -0,0 +1,17 @@
+// Copyright Team AZ. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AZ_MHW/Widget/AZWidget.h"
+#include "AZWidget_PopupDamageInfo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AZ_MHW_API UAZWidget_PopupDamageInfo : public UAZWidget
+{
+	GENERATED_BODY()
+	
+};
